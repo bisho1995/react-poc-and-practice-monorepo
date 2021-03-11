a20be84d6fbbd2d956f2c8ae6870c367eb2c8351
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2021-03-11 09:24:26 +0530

    initial commit

---
+++ apps/react-spring-202103-1/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'react-spring-202103-1',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/react-spring-202103-1',
+};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/react-spring-202103-1'],
+};
