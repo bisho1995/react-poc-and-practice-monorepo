59069525dbb2bb23f589be213bcda2fa45a855ff
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2021-03-11 13:30:42 +0530

    feat: scaffold of three fibre

---
+++ apps/202103-react-three-fibre-1/src/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './app.module.scss';
+
+import { ReactComponent as Logo } from './logo.svg';
+import star from './star.svg';
+
+import { Route, Link } from 'react-router-dom';
+
+export function App() {
+  return <div>Threejs</div>;
+}
+
+export default App;
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  projects: ['<rootDir>/apps/react-spring-202103-1'],
+  projects: [
+    '<rootDir>/apps/react-spring-202103-1',
+    '<rootDir>/apps/202103-react-three-fibre-1',
+  ],
 };
