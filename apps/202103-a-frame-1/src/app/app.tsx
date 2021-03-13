c757ab5c65c30c408b2a6e6bc934d40529d52130
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2021-03-13 22:34:07 +0530

    feat: scaffold a-frame

---
+++ apps/202103-a-frame-1/src/app/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './app.module.scss';
+
+import { ReactComponent as Logo } from './logo.svg';
+import star from './star.svg';
+
+export function App() {
+  return <div>Working?</div>;
+}
+
+export default App;
--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/apps/react-spring-202103-1',
     '<rootDir>/apps/202103-react-three-fibre-1',
     '<rootDir>/apps/202103-ar-1',
+    '<rootDir>/apps/202103-a-frame-1',
   ],
 };
