af619408db4e988195eb7124a84de65007e6523a
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2021-03-13 19:57:25 +0530

    feat: added other files in the app

--- apps/202103-ar-1/src/app/Advanced/Design1/Particles.tsx
+++ apps/202103-ar-1/src/app/Advanced/Design1/Particles.tsx
@@ -26,13 +26,6 @@ export default function Particles({ count, mouse }) {
   }, [count]);
   // The innards of this hook will run every frame
   useFrame((state) => {
-    // Makes the light follow the mouse
-    light &&
-      light.current.position.set(
-        mouse.current[0] / aspect,
-        -mouse.current[1] / aspect,
-        0
-      );
     // Run through the randomized data to calculate some movement
     particles.forEach((particle, i) => {
       let { t, factor, speed, xFactor, yFactor, zFactor } = particle;
@@ -40,24 +33,31 @@ export default function Particles({ count, mouse }) {
       t = particle.t += speed / 2;
       const a = Math.cos(t) + Math.sin(t * 1) / 10;
       const b = Math.sin(t) + Math.cos(t * 2) / 10;
-      const s = Math.cos(t);
-      particle.mx += (mouse.current[0] - particle.mx) * 0.01;
-      particle.my += (mouse.current[1] * -1 - particle.my) * 0.01;
+      const s = 0.3;
+      // particle.mx += (mouse.current[0] - particle.mx) * 0.01;
+      // particle.my += (mouse.current[1] * -1 - particle.my) * 0.01;
+
       // Update the dummy object
-      dummy.position.set(
+
+      const x =
         (particle.mx / 10) * a +
-          xFactor +
-          Math.cos((t / 10) * factor) +
-          (Math.sin(t * 1) * factor) / 10,
+        xFactor +
+        Math.cos((t / 10) * factor) +
+        (Math.sin(t * 1) * factor) / 10;
+
+      const y =
         (particle.my / 10) * b +
-          yFactor +
-          Math.sin((t / 10) * factor) +
-          (Math.cos(t * 2) * factor) / 10,
+        yFactor +
+        Math.sin((t / 10) * factor) +
+        (Math.cos(t * 2) * factor) / 10;
+
+      const z =
         (particle.my / 10) * b +
-          zFactor +
-          Math.cos((t / 10) * factor) +
-          (Math.sin(t * 3) * factor) / 10
-      );
+        zFactor +
+        Math.cos((t / 10) * factor) +
+        (Math.sin(t * 3) * factor) / 10;
+
+      dummy.position.set(x, y, z);
       dummy.scale.set(s, s, s);
       dummy.rotation.set(s * 5, s * 5, s * 5);
       dummy.updateMatrix();
@@ -70,7 +70,7 @@ export default function Particles({ count, mouse }) {
     <>
       <pointLight ref={light} distance={40} intensity={8} color="lightblue" />
       <instancedMesh ref={mesh} args={[null, null, count]}>
-        <dodecahedronBufferGeometry args={[0.2, 0]} />
+        <dodecahedronBufferGeometry args={[0.9, 0]} />
         <meshPhongMaterial color="#050505" />
       </instancedMesh>
     </>
--- apps/202103-ar-1/src/app/Advanced/Design1/index.tsx
+++ apps/202103-ar-1/src/app/Advanced/Design1/index.tsx
@@ -1,10 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Canvas } from 'react-three-fiber';
 import Number from './Number';
 import Particles from './Particles';
+import Sparks from './Sparks';
+import Effects from './Effects';
 
 export default function Design1(): JSX.Element {
   const mouse = useRef([0, 0]);
+  const [down, set] = useState(false);
 
   return (
     <Canvas>
@@ -12,6 +15,19 @@ export default function Design1(): JSX.Element {
       <fog attach="fog" args={['white', 50, 190]} />
       <Number />
       <Particles count={100} mouse={mouse} />
+      <Sparks
+        count={20}
+        mouse={mouse}
+        colors={[
+          '#A2CCB6',
+          '#FCEEB5',
+          '#EE786E',
+          '#e0feff',
+          'lightpink',
+          'lightblue',
+        ]}
+      />
+      <Effects down={down} />
     </Canvas>
   );
 }
